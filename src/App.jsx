de61df27da566a70d1d53dd5a3ef82baa70eec21
Leon Bryan <leonard.bryan.email@gmail.com>
2026-03-08 15:53:17 +0800

    Added pagination buttons (Prev and Next)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useEffect, useState } from 'react';
 import ShopCard from '@/components/ShopCard';
+import { Button } from '@/components/ui/button';
 
 function App() {
   const [shops, setShops] = useState([]);
+  const [isLoading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
+  const [isError, setError] = useState();
+  const limit = 5
 
   useEffect(() => {
     const getData = async () => {
-      const fetched = await fetch('https://veepsh.onrender.com?limit=10');
+      const fetched = await fetch(`https://veepsh.onrender.com?limit=${limit}&page=${page}`);
       let apiJson = await fetched.json();
 
       setShops(apiJson.data)
@@ -14,12 +19,13 @@ function App() {
 
     getData();
 
-  }, []);
+  }, [page]);
 
 
   return (
-    <div className='min-h-screen'>
-      <ul className=" border flex flex-wrap gap-10 justify-center py-10">
+    <div className='min-h-screen flex flex-col'>
+      {/* MAIN CARDS */}
+      <ul className="flex flex-wrap gap-10 justify-center py-10">
         {
           shops.map(item => (
             <li key={item.id} className='relative w-100'>
@@ -28,10 +34,25 @@ function App() {
           ))
         }
       </ul>
+      {/* PAGINATION CONTROLS */}
+      <div className='flex justify-center gap-5 pb-10'>
+        <Button
+          onClick={() => setPage(prev => Math.max(prev - 1, 1))}
+          disabled={page === 1}
+          variant="outline" className="rounded-full! h-15 w-15">&lt;</Button>
+        <Button
+          onClick={() => setPage(prev => Math.max(prev + 1, 1))}
+          disabled={page === 6}
+          variant="outline" className="rounded-full! h-15 w-15">&gt;</Button>
+      </div>
     </div>
   )
 }
 
 export default App
 
-//TODO
+/*
+
+TODO
+ - Pagination
+*/

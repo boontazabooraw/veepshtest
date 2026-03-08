32c6f1bcf029eeba45559b222c0f0bb46af64fd5
Leon Bryan <leonard.bryan.email@gmail.com>
2026-03-08 16:57:23 +0800

    Added loading state for each card

--- src/App.jsx
+++ src/App.jsx
@@ -6,45 +6,55 @@ function App() {
   const [shops, setShops] = useState([]);
   const [isLoading, setLoading] = useState(false);
   const [page, setPage] = useState(1);
-  const [isError, setError] = useState();
+  const [error, setError] = useState();
   const limit = 5
 
   useEffect(() => {
     const getData = async () => {
-      const fetched = await fetch(`https://veepsh.onrender.com?limit=${limit}&page=${page}`);
-      let apiJson = await fetched.json();
+      setLoading(true);
+      try {
+        const fetched = await fetch(`https://veepsh.onrender.com?limit=${limit}&page=${page}`);
+        let apiJson = await fetched.json();
 
-      setShops(apiJson.data)
+        setShops(apiJson.data);
+      } catch (err) {
+        setError(err);
+        console.error(error)
+      } finally {
+        setLoading(false);
+      }
     }
-
     getData();
 
   }, [page]);
 
 
   return (
-    <div className='min-h-screen flex flex-col'>
+    <div className='min-h-screen relative'>
       {/* MAIN CARDS */}
       <ul className="flex flex-wrap gap-10 justify-center py-10">
         {
           shops.map(item => (
             <li key={item.id} className='relative w-100'>
-              <ShopCard long={item.longitude} lat={item.latitude} shopName={item.name} address={item.address} />
+              <ShopCard long={item.longitude} lat={item.latitude} shopName={item.name} address={item.address}
+                loading={`${isLoading ? "opacity-30" : "opacity-100"}`}
+              />
             </li>
           ))
         }
+        {/* PAGINATION CONTROLS */}
+        <div className='flex justify-center gap-5 w-full'>
+          <Button
+            onClick={() => setPage(prev => Math.max(prev - 1, 1))}
+            disabled={page === 1}
+            variant="outline" className="rounded-full! h-15 w-15">&lt;</Button>
+          <Button
+            onClick={() => setPage(prev => Math.max(prev + 1, 1))}
+            disabled={page === 6}
+            variant="outline" className="rounded-full! h-15 w-15">&gt;</Button>
+        </div>
       </ul>
-      {/* PAGINATION CONTROLS */}
-      <div className='flex justify-center gap-5 pb-10'>
-        <Button
-          onClick={() => setPage(prev => Math.max(prev - 1, 1))}
-          disabled={page === 1}
-          variant="outline" className="rounded-full! h-15 w-15">&lt;</Button>
-        <Button
-          onClick={() => setPage(prev => Math.max(prev + 1, 1))}
-          disabled={page === 6}
-          variant="outline" className="rounded-full! h-15 w-15">&gt;</Button>
-      </div>
+
     </div>
   )
 }
--- src/components/ShopCard.jsx
+++ src/components/ShopCard.jsx
@@ -1,9 +1,9 @@
 import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "@/components/ui/card"
 import { Map, MapMarker, MarkerContent } from "@/components/ui/map"
 
-const ShopCard = ({ long, lat, shopName, address }) => {
+const ShopCard = ({ long, lat, shopName, address, loading }) => {
     return (
-        <Card className="relative mx-auto w-full pt-0 overflow-hidden pointer-events-none">
+        <Card className={`relative mx-auto w-full pt-0 overflow-hidden pointer-events-none transition-all duration-300 ${loading}`}>
             {
                 long != null || lat != null ?
                     (
@@ -11,8 +11,8 @@ const ShopCard = ({ long, lat, shopName, address }) => {
                             <MapMarker longitude={long} latitude={lat}>
                                 <MarkerContent className="relative">
                                     {/* <div className="absolute bg-red-300 size-1" /> */}
-                                    <div className="absolute size-6 rounded-full bg-red-400 transform -translate-y-8 -translate-x-2.5"/>
-                                    <div className="absolute size-3 bg-red-400 rotate-45 skew-20 transform -translate-y-4 -translate-x-1"/>
+                                    <div className="absolute size-6 rounded-full bg-red-400 transform -translate-y-8 -translate-x-2.5" />
+                                    <div className="absolute size-3 bg-red-400 rotate-45 skew-20 transform -translate-y-4 -translate-x-1" />
                                 </MarkerContent>
                             </MapMarker>
                         </Map>)
